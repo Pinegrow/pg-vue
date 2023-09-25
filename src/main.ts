86468a50e1d99aacf4621ed8782aadd57015da1c
Akayy <techakayy@gmail.com>
2023-09-25 22:54:26 +1000

    tsconfig updated

--- src/main.ts
+++ src/main.ts
@@ -12,17 +12,16 @@
 import 'uno.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { createHead } from '@vueuse/head'
+import pinia from '@/plugins/pinia'
+import head from '@/plugins/head'
 
 import App from '@/App.vue'
 import router from '@/router'
 
 const app = createApp(App)
-const head = createHead()
 
-app.use(createPinia())
 app.use(router)
+app.use(pinia)
 app.use(head)
 
 app.mount('#app')
