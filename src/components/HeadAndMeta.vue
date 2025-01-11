0991856f2acb3abbfda0959faba021c0d6cc7b5b
Akayy <techakayy@gmail.com>
2025-01-11 13:42:31 +1100

    Included utils

--- src/components/HeadAndMeta.vue
+++ src/components/HeadAndMeta.vue
@@ -7,7 +7,7 @@
 
   import site from '@/site'
   import image from '@/screenshots/image.jpg'
-  // import { pg_font_urls } from '~~/themes/pg-tailwindcss/tokens.mjs'
+  import { fontUrls } from '@/utils/font'
 
   // type TurboScript = Script & { once: true }
 
@@ -20,25 +20,25 @@
   ]
   const noscript: any = []
 
-  // if (pg_font_urls.length) {
-  //   const googleapis = 'https://fonts.googleapis.com'
-  //   const gstatic = 'https://fonts.gstatic.com'
-  //   link.push(
-  //     { rel: 'dns-prefetch', href: googleapis },
-  //     { rel: 'dns-prefetch', href: gstatic },
-  //     { rel: 'preconnect', crossorigin: 'anonymous', href: googleapis },
-  //     { rel: 'preconnect', crossorigin: 'anonymous', href: gstatic },
-  //     {
-  //       rel: 'preload',
-  //       as: 'style',
-  //       onload: "this.onload=null;this.rel='stylesheet'",
-  //       href: pg_font_urls.toString(),
-  //     },
-  //   )
-  //   noscript.push(
-  //     `<link rel="stylesheet" crossorigin="anonymous" href="${pg_font_urls.toString()}" />`,
-  //   )
-  // }
+  if (fontUrls.length) {
+    const googleapis = 'https://fonts.googleapis.com'
+    const gstatic = 'https://fonts.gstatic.com'
+    link.push(
+      { rel: 'dns-prefetch', href: googleapis },
+      { rel: 'dns-prefetch', href: gstatic },
+      { rel: 'preconnect', crossorigin: 'anonymous', href: googleapis },
+      { rel: 'preconnect', crossorigin: 'anonymous', href: gstatic },
+      {
+        rel: 'preload',
+        as: 'style',
+        onload: "this.onload=null;this.rel='stylesheet'",
+        href: fontUrls.toString(),
+      },
+    )
+    noscript.push(
+      `<link rel="stylesheet" crossorigin="anonymous" href="${fontUrls.toString()}" />`,
+    )
+  }
 
   const { title, description, url, author } = site
   const route = useRoute()
